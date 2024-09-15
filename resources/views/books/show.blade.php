76fcd7778711ff96bf6a38fa0fe0075358cf4f43
Claudiane Rodrigues <cra@discente.ifpe.edu.br>
2024-09-15 18:29:33 -0300

    Pequeno ajuste na visualização das capas

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -2,10 +2,16 @@
     use \Illuminate\Support\Facades\Storage;
 @endphp
 @extends('layouts.app')
-
+<style>
+    .limit {
+        width: 300px;
+        height: auto;
+        max-width: 100%;
+    }
+</style>
 @section('content')
     <div class="container">
-        <img src="{{ Storage::url($book->cover) }}" >
+        <img src="{{ Storage::url($book->cover) }}" class="limit">
         <h1>{{ $book->title }}</h1>
         <p><strong>Autor:</strong> {{ $book->author->name }}</p>
         <p><strong>Editora:</strong> {{ $book->publisher->name }}</p>
